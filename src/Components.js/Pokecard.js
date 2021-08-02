8db05381d732f06cdf8560069f055947d14da2cd
Dale <dalesimmons0210@gmail.com>
2021-08-01 18:50:51 -0700

    end 1

--- src/Components.js/Pokecard.js
+++ src/Components.js/Pokecard.js
@@ -4,7 +4,12 @@ const POKE_API = ;
 const Pokecard = () => {
     let imgSrc = `${POKE_API}${props.id}`
     return (
-
+        <div className='Pokecard'>
+            <h1>{props.name}</h1>
+            <img src="imgSrc" alt={props.name}/>
+            <div>Type: {props.type}</div>
+            <div>EXP: {props.exp}</div>
+        </div>
     );
 };
 
