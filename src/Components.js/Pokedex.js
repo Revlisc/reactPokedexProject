ff1cc11d199385abeedb0b3b6f2c694b4f794a54
Dale <dalesimmons0210@gmail.com>
2021-08-03 20:44:17 -0700

    added some styling

--- src/Components.js/Pokedex.js
+++ src/Components.js/Pokedex.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import Pokecard from './Pokecard';
 import { list } from '../List';
+import '../Pokedex-card.css';
 
 const Pokedex = () => {
 
     return (
         <div>
+        
             <h1>Pokedex</h1>
-            {list.map((p) => (
-                <Pokecard id={p.id} name={p.name} type={p.type} exp={p.exp}/>
-            ))}
+            <div className='Pokedex-card'>
+                {list.map((p) => (
+                    <Pokecard id={p.id} name={p.name} type={p.type} exp={p.exp}/>
+                ))}
+            </div>
         </div>
     );
 }
